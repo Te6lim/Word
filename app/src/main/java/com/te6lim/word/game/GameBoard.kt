a7dacffba6eb5e4f9824d35466eecd4c74580995
te6lim <teslimakande@gmail.com>
2022-08-20 14:40:11 +0100

    fixed grid drawing for arbitrary height

--- app/src/main/java/com/te6lim/word/game/GameBoard.kt
+++ app/src/main/java/com/te6lim/word/game/GameBoard.kt
@@ -47,6 +47,20 @@ class GameBoard constructor(context: Context, attributeSet: AttributeSet) : View
         canvas.drawRect(point.x, point.y, right(col), bottom(row), paint)
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+
+        var heightSize = MeasureSpec.getSize(heightMeasureSpec)
+        if (heightMode == MeasureSpec.AT_MOST) {
+            heightSize = if (row >= col) widthSize + ((widthSize / col) * (row - col))
+            else (widthSize / col) * row
+        }
+
+        setMeasuredDimension(widthSize, heightSize)
+    }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
