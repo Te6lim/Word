ece2944c1d58941faacd81d5cf1eafa59bc86901
te6lim <teslimakande@gmail.com>
2022-09-01 18:52:28 +0100

    GameFragment.kt setup

---
+++ app/src/main/java/com/te6lim/word/game/GameFragment.kt
@@ -0,0 +1,15 @@
+package com.te6lim.word.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GameFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
