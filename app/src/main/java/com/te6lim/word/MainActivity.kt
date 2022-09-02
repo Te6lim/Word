f0a6a81c6fd3a98dfca00a4c343b8ad18eeebe93
te6lim <teslimakande@gmail.com>
2022-09-02 22:55:38 +0100

    GameFragment.kt setup

--- app/src/main/java/com/te6lim/word/MainActivity.kt
+++ app/src/main/java/com/te6lim/word/MainActivity.kt
@@ -2,53 +2,10 @@ package com.te6lim.word
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.te6lim.keyboard.KeyBoardView
-import com.te6lim.wordgame.GameBoard
-import com.te6lim.wordgame.WordGame
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val wordGame = WordGame()
-        val gameBoard = findViewById<GameBoard>(R.id.gameBoard)
-
-        gameBoard.setUpWithBoard(wordGame)
-
-        val keyBoard = findViewById<KeyBoardView>(R.id.gameKeyboard)
-
-        with(keyBoard.gameBoardAdapter) {
-            colorCorrect = gameBoard.correctColor
-            colorMisplaced = gameBoard.misplacedColor
-            colorWrong = gameBoard.wrongColor
-        }
-
-        gameBoard.setOnGuessSubmittedListener(object : GameBoard.SubmitListener {
-
-            override fun onGuessSubmitted(correct: List<Char>, misplaced: List<Char>, wrong: List<Char>) {
-                keyBoard.gameBoardAdapter.highlightKeys(correct, misplaced, wrong)
-            }
-
-        })
-
-        keyBoard.setOnKeyClickListener(object : KeyBoardView.OnKeyClickListener {
-
-            override fun onClick(char: Char) {
-                gameBoard.setCharacter(char)
-            }
-
-            override fun onClick(key: KeyBoardView.SpecialKeys) {
-                when (key) {
-                    KeyBoardView.SpecialKeys.ENTER -> {
-                        gameBoard.submitLatestGuess()
-                    }
-
-                    KeyBoardView.SpecialKeys.DELETE -> {
-                        gameBoard.clearLastCharacter()
-                    }
-                }
-            }
-        })
     }
 }
--- app/src/main/java/com/te6lim/word/game/GameFragment.kt
+++ app/src/main/java/com/te6lim/word/game/GameFragment.kt
@@ -4,12 +4,62 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.te6lim.keyboard.KeyBoardView
+import com.te6lim.word.R
+import com.te6lim.word.databinding.FragmentGameBinding
+import com.te6lim.wordgame.GameBoard
+import com.te6lim.wordgame.WordGame
 
 class GameFragment : Fragment() {
+
+    private lateinit var binding: FragmentGameBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_game, container, false)
+
+        val wordGame = WordGame()
+        val gameBoard = binding.gameBoard
+
+        gameBoard.setUpWithBoard(wordGame)
+
+        val keyBoard = binding.keyBoardView
+
+        with(keyBoard.gameBoardAdapter) {
+            colorCorrect = gameBoard.correctColor
+            colorMisplaced = gameBoard.misplacedColor
+            colorWrong = gameBoard.wrongColor
+        }
+
+        gameBoard.setOnGuessSubmittedListener(object : GameBoard.SubmitListener {
+
+            override fun onGuessSubmitted(correct: List<Char>, misplaced: List<Char>, wrong: List<Char>) {
+                keyBoard.gameBoardAdapter.highlightKeys(correct, misplaced, wrong)
+            }
+
+        })
+
+        keyBoard.setOnKeyClickListener(object : KeyBoardView.OnKeyClickListener {
+
+            override fun onClick(char: Char) {
+                gameBoard.setCharacter(char)
+            }
+
+            override fun onClick(key: KeyBoardView.SpecialKeys) {
+                when (key) {
+                    KeyBoardView.SpecialKeys.ENTER -> {
+                        gameBoard.submitLatestGuess()
+                    }
+
+                    KeyBoardView.SpecialKeys.DELETE -> {
+                        gameBoard.clearLastCharacter()
+                    }
+                }
+            }
+        })
+        return binding.root
     }
 }
